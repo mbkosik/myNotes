48281698b6cdb6f7f630c979676df932c361330b
mbkosik <72941144+mbkosik@users.noreply.github.com>
2021-09-19 14:20:18 +0200

    check and remove notes script

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const showModal = (e) => {
     if (e.target === addBtn) {
         addModal.classList.add("visible");
     } else if (e.target === cancelBtn || e.target === addModal) {
-        addModal.classList.remove("visible");
+        removeModal();
     }
 }
 
@@ -16,41 +16,78 @@ window.addEventListener("click", showModal);
 
 // DODAWANIE NOTATKI
 
-const selectedCategory = document.getElementById("category");
-const noteContent = document.getElementById("notes");
 const saveBtn = document.querySelector(".modal__save-btn");
+const deleteBtn = document.querySelector(".btn__delete");
 const notesSection = document.querySelector(".notes");
 
-const addNote = () => {
+const createNote = () => {
+    const noteContent = document.getElementById("notes");
+    const selectedCategory = document.getElementById("category");
     const newNote = document.createElement("div");
-    if (selectedCategory.value === "work") {
-        newNote.classList.add("work");
-    } else if (selectedCategory.value === "grocery") {
-        newNote.classList.add("grocery");
-    } else if (selectedCategory.value === "other") {
-        newNote.classList.add("other");
-    } else if (selectedCategory.value === "choose-category") {
-        document.querySelector(".error").textContent = "Proszę wybrać kategorię notatki";
-        return; 
-    } 
-    newNote.classList.add("note");
-    newNote.innerHTML = `<p>${noteContent.value}</p>`;
-    notesSection.prepend(newNote);
-    addModal.classList.remove("visible");
-    noteContent.value = "";
-    selectedCategory.value = "choose-category";
+    const removeNoteBtn = document.createElement("button");
+    let checking = checkNote(newNote, noteContent, selectedCategory);
+
+    checkNote(newNote, noteContent, selectedCategory);
+    if (checking === "stop") return;
+    addNote(newNote, noteContent, removeNoteBtn);
+    removeModal(noteContent, selectedCategory);
+    removeThisBtn(removeNoteBtn);
 }
 
-saveBtn.addEventListener("click", addNote);
+const checkNote = (note, content, category) => {
+    if (category.value === "work") {
+        note.classList.add("work");  
+    } else if (category.value === "grocery") {
+        note.classList.add("grocery");
+    } else if (category.value === "other") {
+        note.classList.add("other");
+    } else if (category.value === "choose-category") {
+        document.querySelector(".error").textContent = "Proszę wybrać kategorię notatki";
+        return "stop"; 
+    } 
+    if (content.value === "") {
+        document.querySelector(".error").textContent = "Proszę uzupełnić treść notatki";
+        return "stop"; 
+    }
+}
+
+const addNote = (note, content, removeBtn) => {
+    note.classList.add("note");
+    note.innerHTML = `<p>${content.value}</p>`;
+    removeBtn.classList.add("btn");
+    removeBtn.classList.add("btn--in-note");
+    removeBtn.innerHTML = `<i class="fas fa-trash-alt btn__delete-icon"></i>`;
+    note.appendChild(removeBtn);
+    notesSection.prepend(note);
+}
 
 // USUWANIE NOTATEK
 
-const deleteBtn = document.querySelector(".btn__delete");
+// MODALA
+
+const removeModal = (content, category) => {
+    addModal.classList.remove("visible");
+    content.value = "";
+    category.value = "choose-category";
+    document.querySelector(".error").textContent = "";
+}
+
+// WSZYSTKIE NA RAZ
+
 
 const deleteNotes = () => {
     notesSection.innerHTML = "";
 }
 
+// POJEDYNCZE
+
+const removeThisBtn = (inNoteBtn) => {
+    inNoteBtn.addEventListener("click", () => inNoteBtn.parentNode.remove());
+}
+
+saveBtn.addEventListener("click", createNote);
 deleteBtn.addEventListener("click", deleteNotes);
+
+
 
 
